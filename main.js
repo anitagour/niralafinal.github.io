ed13f9e3960b5c49097eac6b3f7d0155429f43c3
anitagour <gouranita81@gmail.com>
2022-09-24 17:25:37 +0530

    Niralaestate

---
+++ main.js
@@ -0,0 +1,51 @@
+const a = document.getElementById("mainHeader");
+console.log(a);
+
+window.addEventListener("scroll", () => {
+  if (200 < scrollY) {
+    a.classList.add("active1");
+  } else if (200 > scrollY) {
+    a.classList.remove("active1");
+  }
+});
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
